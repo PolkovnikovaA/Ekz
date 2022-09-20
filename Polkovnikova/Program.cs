8b4c082e74c86acca31af9f23a27c85f9466b809
полковниковаАВ <@101-04>
2022-09-20 14:05:03 +0300

    Добавьте файлы проекта.

---
+++ Polkovnikova/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polkovnikova
+{
+    /// <summary>
+    /// Основной класс приложения
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Стартовый метод приложения
+        /// </summary>
+        /// <param name="args">
+        /// Аргументы, передаваемые в качестве параметров
+        /// </param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
